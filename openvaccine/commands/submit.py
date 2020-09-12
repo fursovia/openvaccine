ea9522ba9017b9a9890457a3fa9e337b3f4f35af
fursovia <fursov.ia@gmail.com>
2020-09-13 01:17:31 +0300

    ADD: submit command

---
+++ openvaccine/commands/submit.py
@@ -0,0 +1,30 @@
+import typer
+
+import pandas as pd
+
+from openvaccine.utils import load_jsonlines
+
+
+def main(predictions_path: str, output_path: str):
+    preds = load_jsonlines(predictions_path)
+
+    kaggle_preds = []
+    for pred in preds:
+        for i, logits in enumerate(pred["logits"]):
+            kaggle_preds.append(
+                {
+                    "id_seqpos": pred["seq_id"] + f"_{i}",
+                    "reactivity": logits[0],
+                    "deg_Mg_pH10": logits[1],
+                    "deg_pH10": logits[2],
+                    "deg_Mg_50C": logits[3],
+                    "deg_50C": logits[4]
+                }
+            )
+
+    kaggle_preds = pd.DataFrame(kaggle_preds)
+    kaggle_preds.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    typer.run(main)
