c431b0b359d2a56374f3db6f22542da31396ba92
fursovia <fursov.ia@gmail.com>
2020-09-14 00:28:56 +0300

    disable batching

--- bin/predict.sh
+++ bin/predict.sh
@@ -5,12 +5,12 @@ SUBMIT_NAME=$(basename ${LOGDIR})
 
 
 allennlp predict ${LOGDIR}/model.tar.gz \
-    data/test.json \
+    ./data/test.json \
     --output-file ${LOGDIR}/test_preds.json \
     --include-package openvaccine \
     --predictor covid_predictor \
-    --cuda-device 0 \
-    --batch-size 64
+    --cuda-device 0
+    # --batch-size 64
 
 
 PYTHONPATH=. python openvaccine/commands/submit.py \
