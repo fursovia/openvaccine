cf7d3d4c946f577f1ecab0b6eb09754f764546f2
fursovia <fursov.ia@gmail.com>
2020-09-13 23:18:20 +0300

    moved loss

--- openvaccine/__init__.py
+++ openvaccine/__init__.py
@@ -1,3 +1,5 @@
 from .model import CovidClassifier
 from .reader import CovidReader
 from .predictor import CovidPredictor
+from .losses import MCRMSE
+from .utils import load_jsonlines, write_jsonlines
--- openvaccine/model.py
+++ openvaccine/model.py
@@ -7,6 +7,8 @@ from allennlp.models.model import Model
 from allennlp.modules import Seq2SeqEncoder, TextFieldEmbedder
 from allennlp.nn.util import get_text_field_mask
 
+from openvaccine.losses import Loss
+
 
 @Model.register("covid_classifier")
 class CovidClassifier(Model):
@@ -17,6 +19,7 @@ class CovidClassifier(Model):
             structure_field_embedder: TextFieldEmbedder,
             predicted_loop_type_field_embedder: TextFieldEmbedder,
             seq2seq_encoder: Seq2SeqEncoder,
+            loss: Loss,
     ) -> None:
         super().__init__(vocab)
         self._sequence_field_embedder = sequence_field_embedder
@@ -24,20 +27,9 @@ class CovidClassifier(Model):
         self._predicted_loop_type_field_embedder = predicted_loop_type_field_embedder
         self._seq2seq_encoder = seq2seq_encoder
 
-        # we predict reactivity, deg_Mg_pH10, deg_pH10, deg_Mg_50C, deg_50C
+        # we predict reactivity, deg_Mg_pH10, deg_Mg_50C, deg_pH10, deg_50C
         self._linear = torch.nn.Linear(self._seq2seq_encoder.get_output_dim(), 5)
-        self._loss = torch.nn.MSELoss(reduction="none")
-
-    def _calculate_mcrmse(self, logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        # logits: (batch_size, seq_length, 5)
-        logits = logits.reshape((-1, 5))
-        targets = targets.reshape((-1, 5))
-
-        mse = self._loss(logits, targets)
-        mean_mse = torch.mean(mse, dim=0)
-        sqrt_mse = torch.sqrt(mean_mse)
-        loss = torch.mean(sqrt_mse)
-        return loss
+        self._loss = loss
 
     def forward(
             self,
@@ -61,14 +53,13 @@ class CovidClassifier(Model):
 
         logits = self._linear(contextual_embeddings)
 
-        # output_dict = dict(contextual_embeddings=contextual_embeddings, logits=logits, mask=mask)
         output_dict = dict(logits=logits, seq_id=seq_id)
 
         if target is not None:
-            # TODO: take `seq_scored` into account
-            max_length = 68
-            output_dict["loss"] = self._calculate_mcrmse(
-                logits[:, 1:max_length + 1, :],
-                target.transpose(1, 2),
+            # TODO: slicing depends on whether we have start/end tokens
+            target = target.transpose(1, 2)
+            output_dict["loss"] = self._loss(
+                logits[:, 1:target.size(1) + 1, :].reshape((-1, 5)),
+                target.reshape((-1, 5)),
             )
         return output_dict
