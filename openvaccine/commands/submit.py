9a1c371cae4215c16b22e7224d430c9d6bf7f18c
fursovia <fursov.ia@gmail.com>
2020-09-16 18:23:16 +0300

    red echo

--- openvaccine/commands/submit.py
+++ openvaccine/commands/submit.py
@@ -10,7 +10,7 @@ def main(predictions_path: str, output_path: str):
     kaggle_preds = parse_predictions(preds)
 
     kaggle_preds = pd.DataFrame(kaggle_preds)
-    typer.echo(f"Saving submission to {output_path}")
+    typer.secho(f"Saving submission to {output_path}", fg="red")
     kaggle_preds.to_csv(output_path, index=False)
 
 
