9b9a91b1c148797a43ded6af6b27a7352c2f4691
fursovia <fursov.ia@gmail.com>
2020-09-19 23:52:48 +0300

    added arg

--- bin/train.sh
+++ bin/train.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 CONFIG_NAME=$1
+TRAIN_FILENAME=${2:-"train"}
 DATE=$(date +%H%M%S-%d%m)
 EXP_NAME=${DATE}-${CONFIG_NAME}
 
 
-TRAIN_DATA_PATH=./data/train.jsonl \
+TRAIN_DATA_PATH=./data/${TRAIN_FILENAME}.jsonl \
     VALID_DATA_PATH=./data/valid.jsonl \
     allennlp train ./configs/${CONFIG_NAME}.jsonnet \
     --serialization-dir ./logs/${EXP_NAME} \
