25fd71515e35dae6c800953cd4bc9b2154d1fd6a
fursovia <fursov.ia@gmail.com>
2020-09-28 20:53:09 +0300

    bug fix

--- openvaccine/commands/aggregate_preds.py
+++ openvaccine/commands/aggregate_preds.py
@@ -1,10 +1,9 @@
-import json
 from pathlib import Path
 
 import typer
 import numpy as np
 
-from openvaccine.utils.data import write_jsonlines
+from openvaccine.utils.data import write_jsonlines, load_jsonlines
 
 
 def main(log_dir: str, preds_name: str = "test_preds.json"):
@@ -12,15 +11,13 @@ def main(log_dir: str, preds_name: str = "test_preds.json"):
     final_predictions = []
     paths = list(Path(log_dir).iterdir())
     for path in paths:
-        with open(str(path / preds_name)) as f:
-            predictions = json.load(f)
-
-            for i, pred in enumerate(predictions):
-                pred["logits"] = np.array(pred["logits"]) / len(paths)
-                if len(final_predictions) <= i:
-                    final_predictions.append(pred)
-                else:
-                    final_predictions[i]["logits"] += pred["logits"]
+        predictions = load_jsonlines(str(path / preds_name))
+        for i, pred in enumerate(predictions):
+            pred["logits"] = np.array(pred["logits"]) / len(paths)
+            if len(final_predictions) <= i:
+                final_predictions.append(pred)
+            else:
+                final_predictions[i]["logits"] += pred["logits"]
 
     for pred in final_predictions:
         pred["logits"] = pred["logits"].tolist()
