38b077fb5a923b271eb7952e411a97d76ce8e2d6
fursovia <fursov.ia@gmail.com>
2020-10-05 17:04:11 +0300

    fixed paths

--- bin/lm_train.sh
+++ bin/lm_train.sh
@@ -5,8 +5,8 @@ DATE=$(date +%H%M%S-%d%m)
 EXP_NAME=${DATE}-${CONFIG_NAME}
 
 
-LM_TRAIN_DATA_PATH=./data/lm/train.jsonl \
-    LM_VALID_DATA_PATH=./data/lm/valid.jsonl \
+LM_TRAIN_DATA_PATH=./data/external/train.jsonl \
+    LM_VALID_DATA_PATH=./data/external/valid.jsonl \
     allennlp train ./configs/lm/${CONFIG_NAME}.jsonnet \
     --serialization-dir ./logs/${EXP_NAME} \
     --include-package openvaccine
